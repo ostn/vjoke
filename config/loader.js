3fdd17e7552b366b109ae4a04a560885026ff253
ostn <9665466@qq.com>
2019-01-03 17:02:33 +0800

    Tips: Html title修改

--- config/loader.js
+++ config/loader.js
@@ -7,12 +7,12 @@ require.config({
 	waitSeconds: 7,
 	paths: {
 		/* vjoke.cn域名下 加载cdn资源 */
-		'jquery': location.host.indexOf("vjoke") > -1 ? 'https://cdn.bootcss.com/jquery/2.2.2/jquery.min' : 'libs/jquery/jquery-2.2.2.min',
-		'angular': location.host.indexOf("vjoke") > -1 ? 'https://cdn.bootcss.com/angular.js/1.7.3/angular.min' : 'libs/angular/v1.7/angular.min',
-		'angular-animate': location.host.indexOf("vjoke") > -1 ? 'https://cdn.bootcss.com/angular.js/1.7.3/angular-animate.min' : 'libs/angular/v1.7/angular-animate.min',
-		'ui-router': location.host.indexOf("vjoke") > -1 ? 'https://cdn.bootcss.com/angular-ui-router/0.4.3/angular-ui-router.min' : 'libs/router/v0.4.2/ui-router.min',
-		'echarts': location.host.indexOf("vjoke") > -1 ? 'https://cdn.bootcss.com/echarts/3.8.5/echarts.min' : 'libs/echarts/echarts.min',
-		'underscore': location.host.indexOf("vjoke") > -1 ? 'https://cdn.bootcss.com/underscore.js/1.9.1/underscore-min' : 'libs/underscore/v1.9/underscore-min',
+		'jquery': location.host.indexOf("vjoke") > -1 || location.host.indexOf("shenwenhao") > -1 ? 'https://cdn.bootcss.com/jquery/2.2.2/jquery.min' : 'libs/jquery/jquery-2.2.2.min',
+		'angular': location.host.indexOf("vjoke") > -1 || location.host.indexOf("shenwenhao") > -1 ? 'https://cdn.bootcss.com/angular.js/1.7.3/angular.min' : 'libs/angular/v1.7/angular.min',
+		'angular-animate': location.host.indexOf("vjoke") > -1 || location.host.indexOf("shenwenhao") > -1 ? 'https://cdn.bootcss.com/angular.js/1.7.3/angular-animate.min' : 'libs/angular/v1.7/angular-animate.min',
+		'ui-router': location.host.indexOf("vjoke") > -1 || location.host.indexOf("shenwenhao") > -1 ? 'https://cdn.bootcss.com/angular-ui-router/0.4.3/angular-ui-router.min' : 'libs/router/v0.4.2/ui-router.min',
+		'echarts': location.host.indexOf("vjoke") > -1 || location.host.indexOf("shenwenhao") > -1 ? 'https://cdn.bootcss.com/echarts/3.8.5/echarts.min' : 'libs/echarts/echarts.min',
+		'underscore': location.host.indexOf("vjoke") > -1 || location.host.indexOf("shenwenhao") > -1 ? 'https://cdn.bootcss.com/underscore.js/1.9.1/underscore-min' : 'libs/underscore/v1.9/underscore-min',
 		
 		'angular-css': 'libs/angular/angular-css.min',
 		'anim-in-out': 'libs/animate/anim-in-out',
